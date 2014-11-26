930ae757c43ce589196a059d9e1153c44ee6ce42
Ben Saylor <bensaylor@fastmail.fm>
2014-11-26 12:29:19 -0800

    Implement relative neighborhood graph

--- ngcluster/graph.py
+++ ngcluster/graph.py
@@ -3,6 +3,7 @@ Functions for creating neighborhood graphs from expression data
 """
 
 import numpy as np
+from numba import jit
 
 def threshold_graph(data, threshold):
     """
@@ -66,3 +67,46 @@ def nearest_neighbor_graph(data):
         adj[j][i] = True
 
     return adj
+
+def relative_neighborhood_graph(data):
+    """
+    Create a relative neighborhood graph from the given expression data. The
+    correlation coefficient is used as a similarity measure.
+
+    Parameters
+    ----------
+    data : ndarray
+        An n*m array of of expression data for n genes under m conditions.
+
+    Returns
+    -------
+    ndarray
+        An adjacency matrix represented by an n*n array of boolean values.
+    """
+
+    # Compute correlation matrix, replacing NaN (due to all-zero rows) with 0
+    with np.errstate(invalid='ignore'):
+        corr = np.nan_to_num(np.corrcoef(data))
+
+    adj = np.zeros(corr.shape, dtype=bool)
+
+    # TODO: Improvement over this basic algorithm is possible (Supowit 1983)
+    @jit(nopython=True)
+    def build_graph(corr, adj):
+        # For each pair of points (p, q)...
+        for p in range(corr.shape[0] - 1):
+            for q in range(p + 1, corr.shape[0]):
+                # ...if there is no other point z closer to both p and q than
+                # they are to each other...
+                for z in range(corr.shape[0]):
+                    if z == p or z == q:
+                        continue
+                    if corr[p, z] > corr[p, q] and corr[q, z] > corr[p, q]:
+                        # (don't add an edge)
+                        break
+                else:
+                    # ...then add edge (p, q).
+                    adj[p, q] = adj[q, p] = True
+        return adj
+
+    return build_graph(corr, adj)
--- ngcluster/test/test_graph.py
+++ ngcluster/test/test_graph.py
@@ -62,3 +62,72 @@ class TestNearestNeighborGraph(unittest.TestCase):
             [0, 0, 1, 0],
             ])
         assert_array_equal(graph.nearest_neighbor_graph(data), adj)
+
+class TestRelativeNeighborhoodGraph(unittest.TestCase):
+    """ Tests for relative_neighborhood_graph """
+
+    def testRelNeighbor1(self):
+        data = np.array([
+            [1, 2, 3], # a
+            [1, 2, 3], # b
+            [3, 2, 1], # c
+            [3, 2, 1], # d
+            ])
+
+        # (a, b) and (c, d) have correlation 1.
+        # Other pairs have correlation -1.
+        # For all pairs, no other point is more highly correlated with both than
+        # they are to each other. Therefore, all pairs are relative neighbors.
+
+        adj = np.array([
+            [0, 1, 1, 1],
+            [1, 0, 1, 1],
+            [1, 1, 0, 1],
+            [1, 1, 1, 0],
+            ])
+        assert_array_equal(graph.relative_neighborhood_graph(data), adj)
+
+    def testRelNeighbor2(self):
+
+        data = np.array([
+            [1, 1, 3], # a
+            [1, 2, 3], # b
+            [1, 3, 3], # c
+            ])
+
+        # p  q  corr  RN?  why
+        # a  b  0.87   Y   c is closer to a, but not to b
+        # a  c  0.5    N   b is closer to both
+        # b  c  0.87   Y   a is closer to c, but not to b
+
+        adj = np.array([
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0],
+            ])
+        assert_array_equal(graph.relative_neighborhood_graph(data), adj)
+
+    def testRelNeighbor3(self):
+
+        data = np.array([
+            [1, 1, 3], # a
+            [1, 2, 3], # b
+            [1, 2, 3], # c
+            [1, 3, 3], # d
+            ])
+
+        # p  q  corr  RN?  why
+        # a  b  0.87   Y   c is closer to b, but not to a
+        # a  c  0.87   Y   b is closer to c, but not to a
+        # a  d  0.5    N   c is closer to both
+        # b  c  1      Y   no point closer to either
+        # b  d  0.87   Y   c is closer to b, but not to d
+        # c  d  0.87   Y   b is closer to c, but not to d
+
+        adj = np.array([
+            [0, 1, 1, 0],
+            [1, 0, 1, 1],
+            [1, 1, 0, 1],
+            [0, 1, 1, 0],
+            ])
+        assert_array_equal(graph.relative_neighborhood_graph(data), adj)
