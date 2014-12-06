b1f5b7b3e1f60111413a20d16aadf8ef7d8c1a31
Ben Saylor <bensaylor@fastmail.fm>
2014-12-06 11:57:52 -0800

    Add configurations for kmeans and all graph functions

--- ngcluster/config.py
+++ ngcluster/config.py
@@ -4,17 +4,37 @@ Named sets of clustering parameters
 
 from collections import OrderedDict
 
-from ngcluster.cluster import random_clusters, graph_clusters
-from ngcluster.graph import threshold_graph
+from ngcluster.cluster import random_clusters, kmeans, graph_clusters
+from ngcluster.graph import (threshold_graph, nearest_neighbor_graph,
+        relative_neighborhood_graph, gabriel_graph)
 
 configurations = OrderedDict([
-    ('random5', {
-        'description': "5 random clusters",
-        'cluster': (random_clusters, {'k': 5})
+    ('random10', {
+        'description': "10 random clusters",
+        'cluster': (random_clusters, {'k': 10})
+        }),
+    ('kmeans10', {
+        'description': "k-means with 10 clusters",
+        'cluster': (kmeans, {'k': 10})
         }),
     ('threshold_graph_default', {
         'description': "Default threshold graph clustering",
         'cluster': (graph_clusters, {'threshold': 5}),
         'graph': (threshold_graph, {'threshold': 0.85})
-        })
+        }),
+    ('nearest_neighbor_default', {
+        'description': "Default nearest neighbor graph clustering",
+        'cluster': (graph_clusters, {'threshold': 5}),
+        'graph': (nearest_neighbor_graph, {})
+        }),
+    ('relative_neighbor_default', {
+        'description': "Default relative neighbor graph clustering",
+        'cluster': (graph_clusters, {'threshold': 5}),
+        'graph': (relative_neighborhood_graph, {})
+        }),
+    ('gabriel_default', {
+        'description': "Default Gabriel graph clustering",
+        'cluster': (graph_clusters, {'threshold': 5}),
+        'graph': (gabriel_graph, {})
+        }),
     ])
