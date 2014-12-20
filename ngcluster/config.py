0d73248da99d399d56a034b170b7a4a0de128fc9
Ben Saylor <bensaylor@fastmail.fm>
2014-12-20 14:37:36 -0800

    Add configurations for neighbors-of-neighbors and some additional variations

--- ngcluster/config.py
+++ ngcluster/config.py
@@ -31,9 +31,9 @@ configurations = OrderedDict([
     # Vary the following parameters:
     # graph: nearest, relative, gabriel
     # metric: correlation, cosine, euclidean
-    # threshold: 5, 10
-    # iterations: 1, 2
-    # max_clusters: none, 10
+    # threshold
+    # iterations
+    # max_clusters
     # Use graph_clusters_expanding only.
 
     ('nearest_cor_thresh5_iter1_nomax', {
@@ -72,6 +72,160 @@ configurations = OrderedDict([
         'description': "Gabriel graph",
         'graph': (gabriel_graph, {}),
         'cluster': (graph_clusters_expanding, {'threshold': 5, 'iterations': 1}),
+        }),
+
+    # Neighbors-of-neighbors
+    ('nearest_cor_thresh5_iter2_nomax', {
+        'description': "Nearest neighbor graph",
+        'graph': (nearest_neighbor_graph, {'metric': 'correlation'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 5, 'iterations': 2}),
+        }),
+    ('relative_cor_thresh5_iter2_nomax', {
+        'description': "Relative neighborhood graph",
+        'graph': (relative_neighborhood_graph, {'metric': 'correlation'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 5, 'iterations': 2}),
+        }),
+
+    ('nearest_cos_thresh5_iter2_nomax', {
+        'description': "Nearest neighborhood graph",
+        'graph': (nearest_neighbor_graph, {'metric': 'cosine'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 5, 'iterations': 2}),
+        }),
+    ('relative_cos_thresh5_iter2_nomax', {
+        'description': "Relative neighborhood graph",
+        'graph': (relative_neighborhood_graph, {'metric': 'cosine'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 5, 'iterations': 2}),
+        }),
+
+    ('nearest_euc_thresh5_iter2_nomax', {
+        'description': "Nearest neighbor graph",
+        'graph': (nearest_neighbor_graph, {'metric': 'euclidean'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 5, 'iterations': 2}),
+        }),
+    ('relative_euc_thresh5_iter2_nomax', {
+        'description': "Relative neighborhood graph",
+        'graph': (relative_neighborhood_graph, {'metric': 'euclidean'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 5, 'iterations': 2}),
+        }),
+    ('gabriel_euc_thresh5_iter2_nomax', {
+        'description': "Gabriel graph",
+        'graph': (gabriel_graph, {}),
+        'cluster': (graph_clusters_expanding, {'threshold': 5, 'iterations': 2}),
+        }),
+
+    # Nearest neighbor variations
+
+    ('nearest_cor_thresh4_iter1_nomax', {
+        'description': "Nearest neighbor graph, threshold=4",
+        'graph': (nearest_neighbor_graph, {'metric': 'correlation'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 4, 'iterations': 1}),
+        }),
+    ('nearest_cos_thresh4_iter1_nomax', {
+        'description': "Nearest neighbor graph, threshold=4",
+        'graph': (nearest_neighbor_graph, {'metric': 'cosine'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 4, 'iterations': 1}),
+        }),
+    ('nearest_euc_thresh4_iter1_nomax', {
+        'description': "Nearest neighbor graph, threshold=4",
+        'graph': (nearest_neighbor_graph, {'metric': 'euclidean'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 4, 'iterations': 1}),
+        }),
+
+    ('nearest_cor_thresh4_iter3_max15', {
+        'description': "Nearest neighbor graph, threshold=4 max_clusters=15",
+        'graph': (nearest_neighbor_graph, {'metric': 'correlation'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 4, 'iterations': 3,
+            'max_clusters': 15}),
+        }),
+    ('nearest_cos_thresh4_iter3_max15', {
+        'description': "Nearest neighbor graph, threshold=4 max_clusters=15",
+        'graph': (nearest_neighbor_graph, {'metric': 'cosine'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 4, 'iterations': 3,
+            'max_clusters': 15}),
+        }),
+    ('nearest_euc_thresh4_iter3_max15', {
+        'description': "Nearest neighbor graph, threshold=4 max_clusters=15",
+        'graph': (nearest_neighbor_graph, {'metric': 'euclidean'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 4, 'iterations': 3,
+            'max_clusters': 15}),
+        }),
+
+    ('nearest_cor_thresh4_iter5_max15', {
+        'description': "Nearest neighbor graph, threshold=4 max_clusters=15",
+        'graph': (nearest_neighbor_graph, {'metric': 'correlation'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 4, 'iterations': 5,
+            'max_clusters': 15}),
+        }),
+    ('nearest_cos_thresh4_iter5_max15', {
+        'description': "Nearest neighbor graph, threshold=4 max_clusters=15",
+        'graph': (nearest_neighbor_graph, {'metric': 'cosine'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 4, 'iterations': 5,
+            'max_clusters': 15}),
+        }),
+    ('nearest_euc_thresh4_iter5_max15', {
+        'description': "Nearest neighbor graph, threshold=4 max_clusters=15",
+        'graph': (nearest_neighbor_graph, {'metric': 'euclidean'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 4, 'iterations': 5,
+            'max_clusters': 15}),
+        }),
+
+    # Relative neighbor variations
+
+    ('relative_cor_thresh10_iter1_nomax', {
+        'description': "Relative neighborhood graph, threshold=10",
+        'graph': (relative_neighborhood_graph, {'metric': 'correlation'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 10, 'iterations': 1}),
+        }),
+    ('relative_cos_thresh10_iter1_nomax', {
+        'description': "Relative neighborhood graph, threshold=10",
+        'graph': (relative_neighborhood_graph, {'metric': 'cosine'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 10, 'iterations': 1}),
+        }),
+    ('relative_euc_thresh10_iter1_nomax', {
+        'description': "Relative neighborhood graph, threshold=10",
+        'graph': (relative_neighborhood_graph, {'metric': 'euclidean'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 10, 'iterations': 1}),
+        }),
+
+    ('relative_cor_thresh10_iter2_nomax', {
+        'description': "Relative neighborhood graph, threshold=10 iterations=2",
+        'graph': (relative_neighborhood_graph, {'metric': 'correlation'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 10, 'iterations': 2}),
+        }),
+    ('relative_cos_thresh10_iter2_nomax', {
+        'description': "Relative neighborhood graph, threshold=10 iterations=2",
+        'graph': (relative_neighborhood_graph, {'metric': 'cosine'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 10, 'iterations': 2}),
+        }),
+    ('relative_euc_thresh10_iter2_nomax', {
+        'description': "Relative neighborhood graph, threshold=10 iterations=2",
+        'graph': (relative_neighborhood_graph, {'metric': 'euclidean'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 10, 'iterations': 2}),
+        }),
+
+    ('relative_cor_thresh10_iter3_nomax', {
+        'description': "Relative neighborhood graph, threshold=10 iterations=3",
+        'graph': (relative_neighborhood_graph, {'metric': 'correlation'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 10, 'iterations': 3}),
+        }),
+    ('relative_euc_thresh10_iter3_nomax', {
+        'description': "Relative neighborhood graph, threshold=10 iterations=3",
+        'graph': (relative_neighborhood_graph, {'metric': 'euclidean'}),
+        'cluster': (graph_clusters_expanding, {'threshold': 10, 'iterations': 3}),
+        }),
+
+    # Gabriel graph variations
+
+    ('gabriel_euc_thresh4_iter1_nomax', {
+        'description': "Gabriel graph, threshold=4",
+        'graph': (gabriel_graph, {}),
+        'cluster': (graph_clusters_expanding, {'threshold': 4, 'iterations': 1}),
+        }),
+
+    ('gabriel_euc_thresh3_iter1_nomax', {
+        'description': "Gabriel graph, threshold=3",
+        'graph': (gabriel_graph, {}),
+        'cluster': (graph_clusters_expanding, {'threshold': 3, 'iterations': 1}),
         }),
 
     ])
